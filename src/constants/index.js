8cacdecb0f528fc6d10be45dbe3ffcd4c11a0643
Islom <islom@Isloms-MacBook-Air.local>
2025-03-23 15:28:10 +0500

    vazifa yuklandi

--- src/constants/index.js
+++ src/constants/index.js
@@ -15,32 +15,38 @@ export const tables = [
 	{
 		id: 1,
 		name: 'Table 1',
-		image: 'https://cdn.decoist.com/wp-content/uploads/2012/12/Bistro-table-and-chair-set.jpg',
+		image:
+			'https://cdn.decoist.com/wp-content/uploads/2012/12/Bistro-table-and-chair-set.jpg',
 	},
 	{
 		id: 2,
 		name: 'Table 2',
-		image: 'https://cdn.decoist.com/wp-content/uploads/2012/12/Bistro-table-and-chair-set.jpg',
+		image:
+			'https://cdn.decoist.com/wp-content/uploads/2012/12/Bistro-table-and-chair-set.jpg',
 	},
 	{
 		id: 3,
 		name: 'Table 3',
-		image: 'https://cdn.decoist.com/wp-content/uploads/2012/12/Bistro-table-and-chair-set.jpg',
+		image:
+			'https://cdn.decoist.com/wp-content/uploads/2012/12/Bistro-table-and-chair-set.jpg',
 	},
 	{
 		id: 4,
 		name: 'Table 4',
-		image: 'https://cdn.decoist.com/wp-content/uploads/2012/12/Bistro-table-and-chair-set.jpg',
+		image:
+			'https://cdn.decoist.com/wp-content/uploads/2012/12/Bistro-table-and-chair-set.jpg',
 	},
 	{
 		id: 5,
 		name: 'Table 5',
-		image: 'https://cdn.decoist.com/wp-content/uploads/2012/12/Bistro-table-and-chair-set.jpg',
+		image:
+			'https://cdn.decoist.com/wp-content/uploads/2012/12/Bistro-table-and-chair-set.jpg',
 	},
 	{
 		id: 6,
 		name: 'Table 6',
-		image: 'https://cdn.decoist.com/wp-content/uploads/2012/12/Bistro-table-and-chair-set.jpg',
+		image:
+			'https://cdn.decoist.com/wp-content/uploads/2012/12/Bistro-table-and-chair-set.jpg',
 	},
 ]
 export const allFoods = [
@@ -54,19 +60,22 @@ export const allFoods = [
 		id: 2,
 		name: 'Kabob',
 		category: 'Taomlar',
-		image: 'https://www.thespruceeats.com/thmb/S3y5M3h6STDBs4Ojqa1178i1CDE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/kabob-koobideh-5224493-step-08-dcfb8e7bf29245feb70d50e05435dac3.jpg',
+		image:
+			'https://www.thespruceeats.com/thmb/S3y5M3h6STDBs4Ojqa1178i1CDE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/kabob-koobideh-5224493-step-08-dcfb8e7bf29245feb70d50e05435dac3.jpg',
 	},
 	{
 		id: 3,
 		name: 'Shorva',
 		category: 'Taomlar',
-		image: 'https://zira.uz/wp-content/uploads/2019/12/sup-s-frikadelkami-4.jpg',
+		image:
+			'https://zira.uz/wp-content/uploads/2019/12/sup-s-frikadelkami-4.jpg',
 	},
 	{
 		id: 4,
 		name: 'Mastava',
 		category: 'Taomlar',
-		image: 'https://www.shutterstock.com/shutterstock/photos/1373347577/display_1500/stock-photo-mastava-uzbek-rice-soup-on-white-table-1373347577.jpg',
+		image:
+			'https://www.shutterstock.com/shutterstock/photos/1373347577/display_1500/stock-photo-mastava-uzbek-rice-soup-on-white-table-1373347577.jpg',
 	},
 	{
 		id: 5,
@@ -90,7 +99,8 @@ export const allFoods = [
 		id: 8,
 		name: 'Achchiq-chuchuk',
 		category: 'Salatlar',
-		image: 'https://ropovka.com/l/recipe/172/image/1569500719688_24c94b11f2f59b3eb25c14be572dfffd.webp',
+		image:
+			'https://ropovka.com/l/recipe/172/image/1569500719688_24c94b11f2f59b3eb25c14be572dfffd.webp',
 	},
 	{
 		id: 9,
@@ -108,7 +118,8 @@ export const allFoods = [
 		id: 11,
 		name: 'Pepsi',
 		category: 'Ichimliklar',
-		image: 'https://static.insales-cdn.com/images/products/1/422/447578534/00573808.n_1_600x600.png',
+		image:
+			'https://static.insales-cdn.com/images/products/1/422/447578534/00573808.n_1_600x600.png',
 	},
 	{
 		id: 12,
